8f652c4321eb57d46e12d8e2f9941075c5cc9e17
Macktzy <rakusama0@gmail.com>
2026-09-19 01:09:18 +0800

    fix: hide user profile menu on mobile tourist header

--- src/components/tourist/tourist-header.tsx
+++ src/components/tourist/tourist-header.tsx
@@ -236,7 +236,7 @@ export function TouristHeader({
 
           {/* User Profile or Login */}
           {user ? (
-            <div className="relative" ref={userMenuRef}>
+            <div className="relative hidden md:block" ref={userMenuRef}>
               <button
                 type="button"
                 onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
